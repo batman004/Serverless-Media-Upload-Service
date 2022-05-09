d4add5d155226442ebf0df344aa89659beffd36f
batman004 <pathania.yuvraj@gmail.com>
2022-05-10 02:12:17 +0530

    fix : get env vars from os #1

--- api/upload/config.py
+++ api/upload/config.py
@@ -7,9 +7,9 @@ config = dotenv_values(".env")
 
 bucket_name = "backend-lambda-bucket"
 
-os.environ['AWS_ACCESS_KEY_ID'] = config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS_SECRET_ACCESS_KEY']
-os.environ['AWS_DEFAULT_REGION'] = config['AWS_DEFAULT_REGION']
+os.environ.get('AWS_ACCESS_KEY_ID')
+os.environ.get('AWS_SECRET_ACCESS_KEY')
+os.environ.get('AWS_DEFAULT_REGION')
 
 my_config = Config(
     region_name='ap-south-1',
